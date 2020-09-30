bb94e0a14d3e18dc82b6aa5dcb21f2c8c3823b13
shivaAllu <shiva.a1995@gmail.com>
2020-09-30 12:10:07 +0530

    Commiting changes using junit

---
+++ MavenProject/src/test/java/com/cg/Project/AppTest.java
@@ -0,0 +1,23 @@
+package com.cg.Project;
+
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void test1() {
+		System.out.println("Test 1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("Test 2");
+	}
+	@Test
+	public void test3() {
+		System.out.println("Test 3");
+	}
+	@Test
+	public void test4() {
+		System.out.println("Test 4");
+	}
+
+}
